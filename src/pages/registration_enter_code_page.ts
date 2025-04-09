c799177141cf3bbd719182a76cbc12f3cb58031c
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2025-04-09 17:34:07 +0200

    Automate support extension in the interactive installation (HA)

--- src/pages/registration_enter_code_page.ts
+++ src/pages/registration_enter_code_page.ts
@@ -3,7 +3,8 @@ import { type Page } from "puppeteer-core";
 export class RegistrationEnterCodePage {
   private readonly page: Page;
   private readonly codeInput = () => this.page.locator("input#key");
-  private readonly registertButton = () => this.page.locator("button[form='productRegistration']");
+  private readonly codeHAInput = () => this.page.locator("input[id='reg-code-sle-ha-16.0']");
+  private readonly registertButton = () => this.page.locator("button::-p-text(Register)");
 
   constructor(page: Page) {
     this.page = page;
@@ -11,6 +12,10 @@ export class RegistrationEnterCodePage {
 
   async fillCode(code: string) {
     await this.codeInput().fill(code);
+  }
+
+  async fillCodeHA(code: string) {
+    await this.codeHAInput().fill(code);
   }
 
   async register() {
